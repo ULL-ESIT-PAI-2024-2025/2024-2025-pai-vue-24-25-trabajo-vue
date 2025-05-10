8746dad3669a37fd2e0a3d448e6e7b46ef15e345
AlvaroPerezRamos <alu0101574042@ull.edu.es>
2025-05-10 16:24:57 +0000

    Docuamntado

---
+++ src/vue-Figuras/src/App.vue
@@ -0,0 +1,30 @@
+<!--  
+Universidad de La Laguna
+Escuela Superior de Ingeniería y Tecnología
+Grado en Ingeniería Informática
+Programación de Aplicaciones Interactivas
+
+@author Álvaro Pérez Ramos
+@file App.vue
+@since 10 MAY 2025
+@description Componente principal de la aplicación Vue. Importa el componente CanvasFiguras y lo renderiza en la vista.
+-->
+
+<template>
+  <div id="app">
+    <CanvasFiguras />
+  </div>
+</template>
+
+<!-- Importing the CanvasFiguras component -->
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CanvasFiguras from './components/CanvasFiguras/CanvasFiguras.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    CanvasFiguras
+  }
+});
+</script>
