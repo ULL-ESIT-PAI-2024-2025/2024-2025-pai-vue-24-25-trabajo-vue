04704bda54147388284fdccad8bbb61c6dae7514
Diego Antonio Pi Arteaga <91694143+alu0101493084@users.noreply.github.com>
2025-05-11 17:56:25 +0000

    Fix: Created an inner repo by error

---
+++ src/vue-lissajous/src/components/LissajousApp.vue
@@ -0,0 +1,56 @@
+<!--
+  Universidad de La Laguna
+  Escuela Superior de Ingeniería y Tecnología
+  Grado en Ingeniería Informática
+  Programación de Aplicaciones Interactivas
+
+  Componente que define la app Lissajous
+-->
+<template>
+  <div>
+    <h1>Curvas de Lissajous</h1>
+    <div class="container">
+      <LissajousCanvas ref="canvasComponent" :model="model" />
+      <LissajousControls
+        v-model:variableA="variableA"
+        v-model:variableB="variableB"
+        v-model:phi="phi"
+        :animationOn="animationOn"
+        @toggleAnimation="toggleAnimation"
+      />
+    </div>
+    <a href="index.html" class="back-link">Volver a la página principal</a>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, watch } from 'vue';
+import { LissajousModel } from '../composables/model';
+import LissajousCanvas from './LissajousCanvas.vue';
+import LissajousControls from './LissajousControls.vue';
+
+const variableA = ref(3);
+const variableB = ref(2);
+const phi = ref(1.5);
+const animationOn = ref(false);
+const model = new LissajousModel(variableA.value, variableB.value, phi.value);
+const canvasComponent = ref<InstanceType<typeof LissajousCanvas> | null>(null);
+let animationId = 0;
+
+function toggleAnimation() {
+  animationOn.value = !animationOn.value;
+  if (animationOn.value) animate();
+  else cancelAnimationFrame(animationId);
+}
+
+function animate() {
+  model.animationValuesUpdate();
+  canvasComponent.value?.draw();
+  animationId = requestAnimationFrame(animate);
+}
+
+watch([variableA, variableB, phi], () => {
+  model.setParameters(variableA.value, variableB.value, phi.value);
+  canvasComponent.value?.draw();
+});
+</script>
