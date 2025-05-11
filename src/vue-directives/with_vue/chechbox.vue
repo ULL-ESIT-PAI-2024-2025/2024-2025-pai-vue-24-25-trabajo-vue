dc582ec6f4071c11ae37b06bfa0f378281faa53d
AlvaroPerezRamos <alu0101574042@ull.edu.es>
2025-05-11 23:52:45 +0000

    Feat: create directives codes examples

---
+++ src/vue-directives/with_vue/chechbox.vue
@@ -0,0 +1,24 @@
+<!--
+  Universidad de La Laguna
+  Grado en Ingeniería Informática
+  Asignatura: Programación de Aplicaciones Web
+
+  @author: Álvaro Pérez Ramos
+  @date: 10 MOY 2025
+-->
+  
+<div id="app">
+  <button @click="mostrar = !mostrar">Alternar</button>
+  
+  <p v-if="mostrar">Este contenido es visible</p>
+  <p v-else>Este contenido alternativo es visible</p>
+</div>
+
+<script>
+  new Vue({
+    el: '#app',
+    data: {
+      mostrar: true
+    }
+  })
+</script>
---
+++ src/vue-directives/with_vue/lists.vue
@@ -0,0 +1,27 @@
+<!--
+  Universidad de La Laguna
+  Grado en Ingeniería Informática
+  Asignatura: Programación de Aplicaciones Web
+
+  @author: Álvaro Pérez Ramos
+  @date: 10 MOY 2025
+-->
+  
+<div id="app">
+  <ul>
+    <li v-for="item in items">{{ item.texto }}</li>
+  </ul>
+</div>
+
+<script>
+  new Vue({
+    el: '#app',
+    data: {
+      items: [
+        { texto: 'Elemento 1' },
+        { texto: 'Elemento 2' },
+        { texto: 'Elemento 3' }
+      ]
+    }
+  })
+</script>
