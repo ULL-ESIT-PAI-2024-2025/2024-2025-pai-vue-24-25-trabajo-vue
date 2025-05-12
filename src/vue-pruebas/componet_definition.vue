e120cbfb636d3ea8d1438055f0aa8ed7be2b6912
AlvaroPerezRamos <alu0101574042@ull.edu.es>
2025-05-12 00:18:20 +0000

    componet def

---
+++ src/vue-pruebas/componet_definition.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Hola, {{ name }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Ana'
+    };
+  }
+}
+</script>
+
+<style>
+p {
+  color: teal;
+}
+</style>
